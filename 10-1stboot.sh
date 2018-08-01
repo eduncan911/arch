f8b48ea43cb76719b8a422531dd61e5a9508bb0e
Eric Duncan <me@eduncan911.com>
2018-08-01 15:09:13 -0400

    Add comments to 10-1stboot

--- 10-1stboot.sh
+++ 10-1stboot.sh
@@ -3,6 +3,10 @@
 # Usage:
 #   10-1stboot.sh
 
+echo "setfont to sun12x22"
 setfont /usr/share/kbd/consolefonts/sun12x22.psfu.gz
+
+echo "update time"
 timedatectl set-ntp true
 timedatectl status
+
