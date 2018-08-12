14a75b54b37c77061ca7890fd41040ce8cf91dbe
Eric Duncan <me@eduncan911.com>
2018-08-12 12:57:47 -0400

    Download all the things

--- go.sh
+++ go.sh
@@ -15,6 +15,21 @@ HOST_NAME=${HOST_NAME:="dom0"}
 #   Setup a large font:
 #       setfont /usr/share/kbd/consolefonts/sun12x22.psfu.gz
 
+echo "######################################"
+echo "## downloading scripts"
+echo "######################################"
+#wget -q -O 10-1stboot.sh https://raw.githubusercontent.com/eduncan911/arch-setup/master/10-1stboot.sh
+#wget -q -O 20-partition.sh https://raw.githubusercontent.com/eduncan911/arch-setup/master/20-partition.sh
+#wget -q -O 30-bootstrap.sh https://raw.githubusercontent.com/eduncan911/arch-setup/master/30-bootstrap.sh
+#wget -q -O 40-chroot.sh https://raw.githubusercontent.com/eduncan911/arch-setup/master/40-chroot.sh
+#wget -q -O 50-intel-microcode.sh https://raw.githubusercontent.com/eduncan911/arch-setup/master/50-intel-microcode.sh
+#wget -q -O chroot.sh https://raw.githubusercontent.com/eduncan911/arch-setup/master/chroot.sh
+#chmod 755 *.sh
+pacman --noconfirm -Syu unzip
+wget -qO arch-master.zip https://github.com/eduncan911/arch/archive/master.zip
+unzip arch-master.zip
+
+cd arch-master
 scripts/10-1stboot.sh && \
 scripts/20-partition.sh $(DISK) $(PART_PREFIX) && \
 scripts/30-bootstrap.sh && \
